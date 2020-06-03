d7f7a3a50445e6bd56978b01387153d809fa135f
ozancanonen <51087802+ozancanonen@users.noreply.github.com>
2020-06-03 19:27:55 +0300

    new assets

--- SpiderPlatformer2D/Assets/Scripts/SceneLoader.cs
+++ SpiderPlatformer2D/Assets/Scripts/SceneLoader.cs
@@ -14,6 +14,7 @@ public class SceneLoader : MonoBehaviour
 
     private void Start()
     {
+        Time.timeScale = 1;
         currentSceneNumber = SceneManager.GetActiveScene().buildIndex;   
 
     }
