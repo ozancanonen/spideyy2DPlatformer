d2e14c61564ddfdec0add695934279492b9dd758
ozancanonen <51087802+ozancanonen@users.noreply.github.com>
2020-06-11 14:54:46 +0300

    SpiderBoss charge force made delay with events+ thorn deals damage to ants+ wormling 2 made a prefab and hide animation added

--- SpiderPlatformer2D/Assets/BossChargeAnimation.cs
+++ SpiderPlatformer2D/Assets/BossChargeAnimation.cs
@@ -8,18 +8,14 @@ public class BossChargeAnimation : StateMachineBehaviour
     Rigidbody2D rb;
     public float chargeForce;
     private Vector2 direction;
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody2D>();
         Vector2 target = new Vector2(player.position.x, rb.velocity.y);
         direction = target - (Vector2)animator.transform.position;
         direction.y = 0;
-        rb.AddForce(direction.normalized * chargeForce);
         animator.GetComponent<Boss>().LookAtPlayer();
-    }
-    public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
-    {
         direction.x = -direction.x;
         rb.AddForce(direction.normalized * chargeForce/2);
     }
