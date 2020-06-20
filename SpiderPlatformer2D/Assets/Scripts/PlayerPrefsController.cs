a8e210bf41e6d4a282c284ddc227b1f8d3c2728a
UgurArdin <ugurardin@gmail.com>
2020-06-21 00:32:23 +0300

    Check Point Dealed, Kamikaze remastered

--- SpiderPlatformer2D/Assets/Scripts/PlayerPrefsController.cs
+++ SpiderPlatformer2D/Assets/Scripts/PlayerPrefsController.cs
@@ -4,8 +4,45 @@ using UnityEngine;
 
 public class PlayerPrefsController : MonoBehaviour
 {
+    public Vector2 startingPos;
+    public static bool bornInStartingPos;
+
+    private static PlayerPrefsController instance;
+    public static PlayerPrefsController Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogWarning("There is no PlayerPrefs in the hieararchy");
+            }
+
+            return instance;
+        }
+    }
+
     private void Awake()
     {
-        
+        if (instance == null)
+            instance = this;
+        if(!bornInStartingPos)
+        {
+            PlayerPrefs.SetFloat("PlayerXPos", startingPos.x);
+            PlayerPrefs.SetFloat("PlayerYPos", startingPos.y);
+            bornInStartingPos = true;
+        }
+       
+    }
+
+ 
+    public void SavePosition(float x, float y)
+    {
+        PlayerPrefs.SetFloat("PlayerXPos", x);
+        PlayerPrefs.SetFloat("PlayerYPos", y);
+    }
+
+    public Vector2 LastCheckPoint()
+    {
+        return new Vector2(PlayerPrefs.GetFloat("PlayerXPos"), PlayerPrefs.GetFloat("PlayerYPos"));
     }
 }
