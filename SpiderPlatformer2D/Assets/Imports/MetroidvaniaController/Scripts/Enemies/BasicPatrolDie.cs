3e0c12fc2fa17ae5294b9763c07e49314990e9d3
UgurArdin <ugurardin@gmail.com>
2020-06-20 21:17:38 +0300

    Basic Patrol dies from web

---
+++ SpiderPlatformer2D/Assets/Imports/MetroidvaniaController/Scripts/Enemies/BasicPatrolDie.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPatrolDie : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            GetComponentInParent<BasicPatrol>().Die();
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1200));
+        }
+       
+    }
+}
