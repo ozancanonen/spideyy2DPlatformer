3a94f9b204612bc3baf6177ad92719df3b5064af
ozancanonen <51087802+ozancanonen@users.noreply.github.com>
2020-06-21 01:51:58 +0300

    first level

--- SpiderPlatformer2D/Assets/Scripts/FallPlatform.cs
+++ SpiderPlatformer2D/Assets/Scripts/FallPlatform.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class FallPlatform : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject oldCameraBorder;
+    public GameObject newCameraBorder;
+    public GameObject fallPlatformParticle;
+    public GameObject tempColliderObject;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
 
+            tempColliderObject.SetActive(false);
+            fallPlatformParticle.SetActive(true);
         }
     }
 }
