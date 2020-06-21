01ffca429258ff9cb65c7e12456d925e71e682b0
ozancanonen <51087802+ozancanonen@users.noreply.github.com>
2020-06-21 10:10:00 +0300

    first level progress + hive tiles

--- SpiderPlatformer2D/Assets/Scripts/Dialouge System/Ombie.cs
+++ SpiderPlatformer2D/Assets/Scripts/Dialouge System/Ombie.cs
@@ -7,16 +7,29 @@ public class Ombie : MonoBehaviour, INPC
     public int playersCame { get; set; } = 0;
     bool playerCompleted = false;
     public static int childCount = 0;
+    public GameObject oldVirtualCamera;
+    public GameObject newVirtualCamera;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         playersCame++;
         if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(cameraChangeAfter());
             GetComponent<DialogueTrigger>().TriggerDialogue(playerCompleted, playersCame);
+
+        }
     }
 
+    IEnumerator cameraChangeAfter()
+    {
+        yield return new WaitForSeconds(2f);
+        oldVirtualCamera.SetActive(false);
+        newVirtualCamera.SetActive(true);
+    }
     public void QuestCompleted()
     {
         playerCompleted = true;
+        Web_Projectile.canWeb = true;
     }
     
    
---
+++ SpiderPlatformer2D/Assets/Scripts/ThornKütle.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThornKütle : MonoBehaviour
+{
+    [SerializeField] GameObject damageparticle;
+    [SerializeField] GameObject kütleDestroyByRockParticle;
+    [SerializeField] Transform kütleDestroyPos;
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            DamageParticles(col);
+            Debug.Log(gameObject.name);
+            col.gameObject.GetComponent<PlayerController>().UpdateHealth(10);
+        }
+
+        if (col.gameObject.name == "Warmling")
+        {
+            col.gameObject.GetComponent<WormlingHealth>().GetDamage();
+        }
+        if (col.gameObject.tag == "Pullable")
+        {
+            if (col.gameObject.GetComponent<BeeEnemy>() != null)
+            {
+                col.gameObject.GetComponent<BeeEnemy>().Die();
+                DamageParticles(col);
+            }
+            if (col.gameObject.GetComponent<ifStone>() != null)
+            {
+                //Debug.Log(col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude);
+                //if (col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude > 0.001f)
+                //{
+                    GameObject particle = Instantiate(kütleDestroyByRockParticle, kütleDestroyPos.position, Quaternion.identity);
+                particle.SetActive(true);
+                    Destroy(particle, 3f);
+                    Destroy(gameObject);
+                
+            }
+        }
+        if (col.gameObject.tag == "Grappable")
+        {
+            if (col.gameObject.GetComponent<EnemyPatrolling>() != null)
+            {
+                col.gameObject.GetComponent<EnemyPatrolling>().Die();
+                DamageParticles(col);
+            }
+        }
+    }
+    void DamageParticles(Collision2D col)
+    {
+        ContactPoint2D contact = col.contacts[0];
+        GameObject particle = Instantiate(damageparticle, contact.point, Quaternion.identity);
+        Destroy(particle, 0.7f);
+    }
+}
