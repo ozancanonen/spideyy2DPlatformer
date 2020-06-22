1bfc30962271433a399fdab84b4ca617d6207f66
UgurArdin <ugurardin@gmail.com>
2020-06-22 07:12:41 +0300

    SceneCollider is done

---
+++ SpiderPlatformer2D/Assets/SceneCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneCollider : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+    [SerializeField] float timeDelayForNewScene = 0.5f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerPrefsController.cameraChanged = false;
+            PlayerPrefsController.bornInStartingPos = false;
+            Ombie.npcEnder = false;
+            StartCoroutine(DelayAndLoadScene(timeDelayForNewScene));
+        }
+    }
+
+    IEnumerator DelayAndLoadScene(float time)
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+}
